b5d66172eab765d4dd9f9a63e8c5d4fb7bf5d81e
barii <thebarii@gmail.com>
2018-11-03 22:00:10 +0000

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://barii:Qw21we32@ds151523.mlab.com:51523/devconnector"
+
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const profile = require('./routes/profile');
+const posts = require('./routes/posts');
+
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, {useNewUrlParser: true } )
+  .then(() => console.log("mongodb connected"))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send("hello world");
+});
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`)); 
